816b21f9101c1cccd6ef88cd8194a350dcb755f2
lilwayne101 <714838212@qq.com>
2023-04-24 20:09:15 +0800

    更新

--- python_pycharm/py_opencv_faceReco.py
+++ python_pycharm/py_opencv_faceReco.py
@@ -12,52 +12,54 @@ import cv2
 class FaceDetect:
     def __init__(self):
         # 创建一个级联分类器,用来识别人脸使用
+        self.faceRects = None
         self.faceImg = cv2.imread(r".\Image\LENIN3.jpg")
         self.classifier = cv2.CascadeClassifier()
         # 加载特征文件
-        self.classifier.load(r"F:\anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_alt.xml")
+        self.classifier.load(r"F:\anaconda3\envs\py37\Lib\site-packages\cv2\data\haarcascade_frontalface_alt.xml")
 
     # 人脸矩形区域绘制
     def face_detect(self):
-        faceImg = self.faceImg
+        face_img = self.faceImg
         classifier = self.classifier
         # 识别图像中的人脸  返回矩形区域  使用列表进行存储
-        self.faceRects = classifier.detectMultiScale(faceImg)
+        self.faceRects = classifier.detectMultiScale(face_img)
         # print(faceImg)
         # [[76 31 51 51]]   x y w h
         # print(faceRects)
         for x, y, w, h in self.faceRects:
-            cv2.rectangle(faceImg, (x, y), (x + w, y + h), color=(255, 255, 0), thickness=1)
-        cv2.imshow('img', faceImg)
+            cv2.rectangle(face_img, (x, y), (x + w, y + h), color=(255, 255, 0), thickness=1)
+        cv2.imshow('img', face_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-
     # 绘制logo
     def draw_logo(self, logo):
-        faceRects = self.faceRects
-        faceImg = self.faceImg
-        cv2.imshow("img", faceImg)
+        face_rects = self.faceRects
+        print(face_rects)
+        face_img = self.faceImg
+        cv2.imshow("img", face_img)
         # 计算图像的缩放比例
         ratio = logo.shape[0] / logo.shape[1]
-        smallLogo = None
-        for faceRect in faceRects:
-            faceX = faceRect[0]
-            faceY = faceRect[1]
-            faceW = faceRect[2]
-            faceH = int(faceRect[3] * ratio)
+        small_logo = None
+        for face_rect in face_rects:
+            face_x = face_rect[0]
+            face_y = face_rect[1]
+            face_w = face_rect[2]
+            face_h = int(face_rect[3] * ratio)
             # 因为图像比较大,需要缩放
-            smallLogo = cv2.resize(logo, dsize=(faceW, faceH))
-            smallLogoH, smallLogoW = smallLogo.shape[:2]
-            for row in range(smallLogoW):
-                for col in range(smallLogoH):
-                    faceImg[faceX + int(faceW * 0.8) + row, faceY +faceW * 2 + col] = smallLogo[row, col]
-                    break
-                break
+            small_logo = cv2.resize(logo, dsize=(face_w, face_h))
+            small_logo_w, small_logo_h = small_logo.shape[:2]
+            print(face_img.shape)
+            # print(small_logo.shape)
+            # cv2.imshow("logo", small_logo)
+            for row in range(small_logo_w):
+                for col in range(small_logo_h):
+                    face_img[face_y - small_logo_w + row, face_x + col] = small_logo[row, col]  # 注意：图像和数组维度位置的区别
         # cv2.imshow('smallLogo', smallLogo)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
-        cv2.imshow('img', faceImg)
+        cv2.imshow('img', face_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
