8ab1067706b45e7c76ad046018b4384a87e12139
lilwayne101 <714838212@qq.com>
2023-05-26 17:40:52 +0800

    更新

--- course_code/practice.py
+++ course_code/practice.py
@@ -527,3 +527,38 @@ from torch import optim
 # plt.plot(x, sigmoid)
 # plt.plot(x, d_sigmoid)
 # plt.show()
+
+# softmax
+# x = np.linspace(-10, 10, 10000)
+# softmax = np.exp(x) / np.sum(np.exp(x))
+# plt.plot(x, softmax)
+# plt.show()
+
+# 数据集
+_x = [i * 0.01 for i in range(100)]
+# 回归权重w=5, b=5
+_y = [x * 5 + 5 + random.random() for x in _x]
+# 初始化权重
+w = random.random()
+b = random.random()
+while True:
+    for x, y in zip(_x, _y):
+        # 预测值
+        h = w * x + b
+        # loss
+        loss = (h - y) ** 2
+        # dw, db
+        dw = 2 * x * (h - y)
+        db = 2 * (h - y)
+        # 反向传播,权重更新
+        r = 0.001
+        w -= dw * r
+        b -= db * r
+        plt.pause(0.1)
+        plt.plot(_x, _y, ".")
+        plt.ion()
+        plt.plot(x, w * x + b)
+
+    plt.show()
+
+
